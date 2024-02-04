747686c110544b58ab71ecf4b356cd26503ada42
Ewa Potorecka <p.ewa@wwg.it>
2024-02-04 09:54:43 +0100

    build fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,22 @@
 'use client'
-
-import DancingCat from '@/components/dancingCat/DancingCat'
+import dynamic from 'next/dynamic'
 import Link from 'next/link'
+
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('@/components/dancingCat/DancingCat'),
+  { ssr: false }
+)
+
+
+// import DancingCat from '@/components/dancingCat/DancingCat'
  
 export default function NotFound() {
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
       <h1 className='text-xl'>Well, this is awkward…</h1>
       <Link href="/" className='text-[8px]'>Return Home</Link>
-<DancingCat/>
+<DynamicComponentWithNoSSR/>
 
     </div>
   )
